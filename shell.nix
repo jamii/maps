dabb05a76ca2ca843132a9b47767b037138ba14d
Jamie Brandon <jamie@scattered-thoughts.net>
2024-10-06 14:40:58 -0700

    Add mlc

--- shell.nix
+++ shell.nix
@@ -22,12 +22,36 @@ zig = pkgs.stdenv.mkDerivation {
     '';
 };
 
+mlc = pkgs.stdenv.mkDerivation {
+    name = "mlc";
+    nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+    src = builtins.fetchurl (
+        if (pkgs.system == "x86_64-linux") then {
+            url = "https://downloadmirror.intel.com/834254/mlc_v3.11b.tgz";
+            sha256 = "0918xgpwid4j5wgx96sv0xgj020s60w3kif9ckamkbs5s4kvsnjx";
+        } else
+        throw ("Unknown system " ++ pkgs.system)
+    );
+    unpackPhase = ''
+       tar -xzf $src
+    '';
+    dontConfigure = true;
+    dontBuild = true;
+    dontStrip = true;
+    installPhase = ''
+    mkdir -p $out
+    mkdir -p $out/bin
+    mv ./Linux/mlc $out/bin/mlc
+    '';
+};
+
 in
 
 pkgs.mkShell rec {
     nativeBuildInputs = [
         zig
         pkgs.cargo
+        mlc
     ];
     buildInputs = [
     ];
